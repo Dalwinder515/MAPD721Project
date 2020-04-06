1d2691ba5585e9da7254a332dfb4ccf6ab61adef
Dalwinder Singh <dalwinder.95451996@gmail.com>
2020-04-06 15:49:10 -0400

    Added Bluetooth Enabling/Disabling

--- app/src/main/java/com/ds/mapd721project/HomeScreen.java
+++ app/src/main/java/com/ds/mapd721project/HomeScreen.java
@@ -3,6 +3,7 @@ package com.ds.mapd721project;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -15,10 +16,13 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeScreen extends AppCompatActivity implements BillingProcessor.IBillingHandler {
 
-    Button logout, buy;
+    Button logout, buy, bluetooth_enable, bluetooth_disable;
     FirebaseAuth mAuth;
     FirebaseAuth.AuthStateListener mAuthListener;
     BillingProcessor billingProcessor;
+    BluetoothAdapter bluetoothAdapter;
+    Intent enable_bluetooth;
+    int requestCodebt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +33,12 @@ public class HomeScreen extends AppCompatActivity implements BillingProcessor.IB
         billingProcessor = new BillingProcessor(this, null, this);
         logout = findViewById(R.id.logout);
         buy = findViewById(R.id.buy);
+        bluetooth_enable = findViewById(R.id.bluetooth_enable);
+        bluetooth_disable = findViewById(R.id.bluetooth_disable);
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        enable_bluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        requestCodebt = 24;
     }
 
     public void logout(View view) {
@@ -42,7 +52,14 @@ public class HomeScreen extends AppCompatActivity implements BillingProcessor.IB
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (!billingProcessor.handleActivityResult(requestCode, resultCode, data)) {
+        if(requestCode == requestCodebt)
+        {
+            if(resultCode == RESULT_OK)
+                Toast.makeText(this, "Bluetooth enabled", Toast.LENGTH_SHORT).show();
+            else if(resultCode == RESULT_CANCELED)
+                Toast.makeText(this, "Bluetooth Enabling Cancelled", Toast.LENGTH_SHORT).show();
+        }
+        else if (!billingProcessor.handleActivityResult(requestCode, resultCode, data)) {
             super.onActivityResult(requestCode, resultCode, data);
         }
     }
@@ -73,5 +90,21 @@ public class HomeScreen extends AppCompatActivity implements BillingProcessor.IB
     @Override
     public void onBillingInitialized() {
 
+    }
+
+    public void enable_bluetooth(View view) {
+        if(bluetoothAdapter == null)
+        {
+            Toast.makeText(this, "Device doesn't support Bluetooth", Toast.LENGTH_SHORT).show();
+        }
+        else if(!bluetoothAdapter.isEnabled())
+        {
+            startActivityForResult(enable_bluetooth, requestCodebt);
+        }
+    }
+
+    public void disable_bluetooth(View view) {
+        if(bluetoothAdapter.isEnabled())
+            bluetoothAdapter.disable();
     }
 }
