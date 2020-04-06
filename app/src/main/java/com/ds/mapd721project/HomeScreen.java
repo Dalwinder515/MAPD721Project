b359385ec419bbe83ba3b8a5ad90dd27dc6e7aec
Dalwinder Singh <dalwinder.95451996@gmail.com>
2020-04-06 15:14:25 -0400

    Completed in-app demo purchase (Ref. Git repository)

--- app/src/main/java/com/ds/mapd721project/HomeScreen.java
+++ app/src/main/java/com/ds/mapd721project/HomeScreen.java
@@ -1,11 +1,13 @@
 package com.ds.mapd721project;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.anjlab.android.iab.v3.BillingProcessor;
 import com.anjlab.android.iab.v3.TransactionDetails;
@@ -16,6 +18,7 @@ public class HomeScreen extends AppCompatActivity implements BillingProcessor.IB
     Button logout, buy;
     FirebaseAuth mAuth;
     FirebaseAuth.AuthStateListener mAuthListener;
+    BillingProcessor billingProcessor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +26,7 @@ public class HomeScreen extends AppCompatActivity implements BillingProcessor.IB
         setContentView(R.layout.activity_home_screen);
 
         //mAuth = FirebaseAuth.getInstance();
+        billingProcessor = new BillingProcessor(this, null, this);
         logout = findViewById(R.id.logout);
         buy = findViewById(R.id.buy);
     }
@@ -33,12 +37,27 @@ public class HomeScreen extends AppCompatActivity implements BillingProcessor.IB
     }
 
     public void buy(View view) {
+        billingProcessor.purchase(HomeScreen.this, "android.test.purchased");
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (!billingProcessor.handleActivityResult(requestCode, resultCode, data)) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (billingProcessor != null) {
+            billingProcessor.release();
+        }
+        super.onDestroy();
     }
 
     @Override
     public void onProductPurchased(String productId, TransactionDetails details) {
-
+        Toast.makeText(this, "New Product Purchased", Toast.LENGTH_SHORT).show();
     }
 
     @Override
