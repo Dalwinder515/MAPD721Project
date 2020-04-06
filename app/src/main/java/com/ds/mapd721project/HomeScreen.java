d70592abb522bc98eec355e97347e1e8265bda63
Dalwinder Singh <dalwinder.95451996@gmail.com>
2020-04-06 16:05:44 -0400

    Listing Paired Devices

--- app/src/main/java/com/ds/mapd721project/HomeScreen.java
+++ app/src/main/java/com/ds/mapd721project/HomeScreen.java
@@ -4,15 +4,20 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.anjlab.android.iab.v3.BillingProcessor;
 import com.anjlab.android.iab.v3.TransactionDetails;
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Set;
 
 public class HomeScreen extends AppCompatActivity implements BillingProcessor.IBillingHandler {
 
@@ -23,6 +28,7 @@ public class HomeScreen extends AppCompatActivity implements BillingProcessor.IB
     BluetoothAdapter bluetoothAdapter;
     Intent enable_bluetooth;
     int requestCodebt;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +42,7 @@ public class HomeScreen extends AppCompatActivity implements BillingProcessor.IB
         bluetooth_enable = findViewById(R.id.bluetooth_enable);
         bluetooth_disable = findViewById(R.id.bluetooth_disable);
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        listView = findViewById(R.id.deviceList);
 
         enable_bluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
         requestCodebt = 24;
@@ -106,5 +113,22 @@ public class HomeScreen extends AppCompatActivity implements BillingProcessor.IB
     public void disable_bluetooth(View view) {
         if(bluetoothAdapter.isEnabled())
             bluetoothAdapter.disable();
+    }
+
+    public void showDevices(View view) {
+        Set<BluetoothDevice> bd = bluetoothAdapter.getBondedDevices();
+        String[] devices = new String[bd.size()];
+        int index = 0;
+
+        if(bd.size() > 0)
+        {
+            for(BluetoothDevice device:bd)
+            {
+                devices[index] = device.getName();
+                index++;
+            }
+            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, devices);
+            listView.setAdapter(arrayAdapter);
+        }
     }
 }
